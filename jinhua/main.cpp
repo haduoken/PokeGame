e6e72734f03ab1119cfea9675f9f02403e831770
yikir <yakirzhang@foxmail.com>
2017-08-18 00:36:59 +0800

    Initial Commit

---
+++ jinhua/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jinhua
+//
+//  Created by 张义杰 on 2017/8/18.
+//  Copyright © 2017年 张义杰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
